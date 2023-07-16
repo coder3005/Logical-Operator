55b7254c38ae816200cce44a68a15bf76735d06a
ABHI GUPTA <138576028+coder3005@users.noreply.github.com>
2023-07-16 20:59:18 +0530

    Create Logical.java

---
+++ Logical.java
@@ -0,0 +1,11 @@
+/* Logical Operator Example...! */
+class Logical
+{
+    public static void main(String[] args) {
+        System.out.println("Logical AND");
+        System.out.println((10>5) && (2>1));
+        System.out.println((10>5) && (2<1));
+        System.out.println((10<5) && (2<1));
+
+    }
+}
